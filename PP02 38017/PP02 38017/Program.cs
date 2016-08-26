e8ab9ec0fa47b5709d9b3d4ee28bf3b78501bf65
Rodrigo Arnaldo de Araújo <rodrigo.a.araujo@hotmail.com>
2016-08-26 02:48:04 -0300

    Add files via upload
    Elabore um programa em C# que solicita que o usuário digite o valor do preço de um produto.
    Depois, deve solicitar que o usuário digite quantas unidades desse produto ele deseja comprar.
    O programa deve calcular o preço total da compra e exibir esse valor.
    Caso esse valor seja até R$ 500,00, o pagamento deverá ser a vista e isso deve ser informado ao usuário.
    Caso esse valor seja acima de R$ 500,00, o pagamento poderá ser parcelado em até 3 vezes. Nesse caso, o programa deve solicitar que o usuário escolha em quantas parcelas ele gostaria de pagar.
    Se for em uma parcela, o valor sofrerá um desconto de 20%.
    Se for em 2 parcelas, o valor sofrerá um desconto de 15%.
    Se for em 3 parcelas, o valor sofrerá um desconto de 12%.
    Utilize switch/case para comparar o número de parcelas.
    Para cada caso, informe ao usuário qual será o valor da parcela, considerando o desconto respectivo.
    Se for um número diferente de 1 a 3, informe que o usuário deverá tentar novamente. (n

---
+++ PP02 38017/PP02 38017/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP02_38017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float parcela;
+            float valor;
+            float quantia;
+
+            Console.WriteLine("Digite o valor do produto: ");
+            valor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a unidade de produtos que deseja comprar: ");
+            quantia = float.Parse(Console.ReadLine());
+            float calculaQta = quantia * valor;
+            float total;
+            int op;
+
+            if (calculaQta <= 500.00)
+            {
+
+                total = calculaQta * 0.8f;
+                Console.WriteLine("\nO pagamento devera ser feito à vista, o valor de sua compra é: {0:C}", calculaQta);
+                Console.WriteLine("E recebeu um desconto de 20% total a pagar é: {0:C}", total);
+            }
+            else
+            {
+
+                Console.WriteLine("Escolha o modo de pagamento:");
+                Console.WriteLine("1 - A vista");
+                Console.WriteLine("2 - Parcelado em x2");
+                Console.WriteLine("3 - Parcelado em x3");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+
+                    case 1:
+
+                        total = calculaQta * 0.8f;
+                        Console.WriteLine("\nO valor de sua compra é: {0:C}", calculaQta);
+                        Console.WriteLine("E recebeu um desconto de 20% valor total a pagar: {0:C}", total);
+
+                        break;
+
+                    case 2:
+
+                        total = calculaQta * 0.85f;
+                        parcela = total / 2;
+                        Console.WriteLine("\nO valor de sua compra é: {0:C}", calculaQta);
+                        Console.WriteLine("E recebeu um desconto de 15% valor total a pagar: {0:C}", total);
+                        Console.WriteLine("\nVocê devera pagar 2x parcelas de: {0:C}", parcela);
+                        break;
+
+                    case 3:
+
+                        total = calculaQta * 0.88f;
+                        parcela = total / 3;
+                        Console.WriteLine("\nO valor de sua compra é: {0:C}", calculaQta);
+                        Console.WriteLine("E recebeu um desconto de 12% valor total a pagar: {0:C}", total);
+                        Console.WriteLine("Você devera pagar 2x parcelas de: {0:C}", parcela);
+                        break;
+
+                    default:
+                        Console.WriteLine("Numero inválido.");
+                        break;
+                }
+
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
